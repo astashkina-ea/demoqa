c15f3bf5a3b58cfd4fc5eca029a3e3e6bf908423
Ekaterina Astaskina <fedosha9615@gmail.com>
2023-03-30 01:47:55 +0300

    added homework

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -7,6 +7,5 @@ public class TestBase {
     static void beforeAll(){
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
     }
 }
