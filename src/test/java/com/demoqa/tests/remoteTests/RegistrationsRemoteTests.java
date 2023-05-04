1b439106f5f56364cfd041708c2189889ccb97e7
Ekaterina Astaskina <fedosha9615@gmail.com>
2023-05-04 20:52:31 +0300

    added remote tests

---
+++ src/test/java/com/demoqa/tests/remoteTests/RegistrationsRemoteTests.java
@@ -0,0 +1,35 @@
+package com.demoqa.tests.remoteTests;
+
+import com.demoqa.data.viewModel.RegistrationFormViewModel;
+import com.demoqa.pages.RegistrationFormPage;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class RegistrationsRemoteTests extends RemoteTestBase {
+
+    RegistrationFormPage registrationFormPage = new RegistrationFormPage();
+    RegistrationFormViewModel registrationFormViewModel = new RegistrationFormViewModel();
+
+    @Test
+    @Tag("remote")
+    void successfulTest() {
+        registrationFormPage.openPage()
+                .removeBanner()
+                .setFirstName(registrationFormViewModel.nameValue)
+                .setLastName(registrationFormViewModel.lastNameValue)
+                .setUserEmail(registrationFormViewModel.emailValue)
+                .setGender(registrationFormViewModel.genderValue)
+                .setUserNumber(registrationFormViewModel.numberValue)
+                .setBirthDate(registrationFormViewModel.dayValue, registrationFormViewModel.monthValue, registrationFormViewModel.yearValue)
+                .setSubject(registrationFormViewModel.subjectValue)
+                .setHobbiesWrapper(registrationFormViewModel.hobbyValue)
+                .uploadPicture(registrationFormViewModel.pathValue)
+                .setAddress(registrationFormViewModel.addressValue)
+                .setState(registrationFormViewModel.stateValue)
+                .setCity(registrationFormViewModel.cityValue)
+                .pressSubmit();
+
+        registrationFormPage.verifyRegistrationResultModalAppears()
+                .verifyResult(registrationFormViewModel);
+    }
+}
