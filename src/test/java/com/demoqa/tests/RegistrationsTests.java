4ab345d85710ba4d6b37d1c7e7cdc677b97a52d2
Ekaterina Astaskina <fedosha9615@gmail.com>
2023-05-04 22:08:17 +0300

    added steps and attach

--- src/test/java/com/demoqa/tests/RegistrationsTests.java
+++ src/test/java/com/demoqa/tests/RegistrationsTests.java
@@ -2,8 +2,11 @@ package com.demoqa.tests;
 
 import com.demoqa.data.viewModel.RegistrationFormViewModel;
 import com.demoqa.pages.RegistrationFormPage;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import static io.qameta.allure.Allure.step;
 
 public class RegistrationsTests extends TestBase {
 
@@ -12,24 +15,30 @@ public class RegistrationsTests extends TestBase {
 
     @Test
     @Tag("remote")
+    @DisplayName("Успешное заполнение формы")
     void successfulTest() {
-        registrationFormPage.openPage()
-                .removeBanner()
-                .setFirstName(registrationFormViewModel.nameValue)
-                .setLastName(registrationFormViewModel.lastNameValue)
-                .setUserEmail(registrationFormViewModel.emailValue)
-                .setGender(registrationFormViewModel.genderValue)
-                .setUserNumber(registrationFormViewModel.numberValue)
-                .setBirthDate(registrationFormViewModel.dayValue, registrationFormViewModel.monthValue, registrationFormViewModel.yearValue)
-                .setSubject(registrationFormViewModel.subjectValue)
-                .setHobbiesWrapper(registrationFormViewModel.hobbyValue)
-                .uploadPicture(registrationFormViewModel.pathValue)
-                .setAddress(registrationFormViewModel.addressValue)
-                .setState(registrationFormViewModel.stateValue)
-                .setCity(registrationFormViewModel.cityValue)
-                .pressSubmit();
-
-        registrationFormPage.verifyRegistrationResultModalAppears()
-                .verifyResult(registrationFormViewModel);
+        step("Открыть форму", () -> {
+            registrationFormPage.openPage()
+                    .removeBanner();
+        });
+        step("Заполнить форму", () -> {
+            registrationFormPage.setFirstName(registrationFormViewModel.nameValue)
+                    .setLastName(registrationFormViewModel.lastNameValue)
+                    .setUserEmail(registrationFormViewModel.emailValue)
+                    .setGender(registrationFormViewModel.genderValue)
+                    .setUserNumber(registrationFormViewModel.numberValue)
+                    .setBirthDate(registrationFormViewModel.dayValue, registrationFormViewModel.monthValue, registrationFormViewModel.yearValue)
+                    .setSubject(registrationFormViewModel.subjectValue)
+                    .setHobbiesWrapper(registrationFormViewModel.hobbyValue)
+                    .uploadPicture(registrationFormViewModel.pathValue)
+                    .setAddress(registrationFormViewModel.addressValue)
+                    .setState(registrationFormViewModel.stateValue)
+                    .setCity(registrationFormViewModel.cityValue)
+                    .pressSubmit();
+        });
+        step("Проверить результат", () -> {
+            registrationFormPage.verifyRegistrationResultModalAppears()
+                    .verifyResult(registrationFormViewModel);
+        });
     }
 }
--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -1,7 +1,12 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.demoqa.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -27,5 +32,18 @@ public class TestBase {
         //Configuration.timeout = 10000;
         //Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
